12dd96dd243a146dbf1af3f60edbe61c72605b75
cem sezeroglu <cemsezeroglu@hotmail.com>
2020-10-16 00:08:23 +0300

    Initial Commit

---
+++ berber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  berber
+//
+//  Created by Cem Sezeroglu on 16.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
